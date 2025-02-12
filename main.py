6b83f9432464f62645a8070b9e5453734d0f2f56
Chekhov <pavel.miridonov123@gmail.com>
2025-02-12 21:42:29 +0400

    2/150

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 # Task 1/150
 # 121. Best Time to Buy and Sell Stock
 
-class Solution:
-    def maxProfit(self, prices):
-        lowest_price = 99999999999999999999
-        max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < lowest_price:
-                lowest_price = prices[i]
-            if prices[i] - lowest_price > max_profit:
-                max_profit = prices[i] - lowest_price
+# class Solution:
+#     def maxProfit(self, prices):
+#         lowest_price = 99999999999999999999
+#         max_profit = 0
+#         for i in range(len(prices)):
+#             if prices[i] < lowest_price:
+#                 lowest_price = prices[i]
+#             if prices[i] - lowest_price > max_profit:
+#                 max_profit = prices[i] - lowest_price
+#
+#         print(max_profit)
+#
+# sol = Solution()
+# sol.maxProfit(prices = [7,1,5,3,6,4])
 
-        print(max_profit)
+# Task 2/150
+# 125. Valid Palindrome
+
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r'[-_.,+=&?<>{}[]|/! \]+', '', s)
+
+        for i in range(int(len(s) / 2)):
+            if s[i] != s[-i - 1]:
+                return False
+        return True
+
 
 sol = Solution()
-sol.maxProfit(prices = [7,1,5,3,6,4])
+print(sol.isPalindrome('ab_a'))
